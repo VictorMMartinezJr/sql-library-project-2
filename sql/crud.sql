6c846699e8fd786695996fd7c1c8a16280909e49
Victor <victorm.martinezjr@outlook.com>
2026-07-09 15:59:28 -0400

    feat: insert nw data into issued_status table and add new book_quality column to return_status table

--- sql/crud.sql
+++ sql/crud.sql
@@ -36,3 +36,21 @@ SELECT b.isbn, b.book_title, COUNT(ist.issued_book_isbn) AS issue_count
 FROM books b
 JOIN issued_status ist ON b.isbn = ist.issued_book_isbn
 GROUP BY b.isbn;
+
+-- ADDING NEW ISSUED STATUS RECORDS
+INSERT INTO issued_status(issued_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id)
+VALUES
+('IS151', 'C118', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL '24 days',  '978-0-553-29698-2', 'E108'),
+('IS152', 'C119', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL '13 days',  '978-0-553-29698-2', 'E109'),
+('IS153', 'C106', 'Pride and Prejudice', CURRENT_DATE - INTERVAL '7 days',  '978-0-14-143951-8', 'E107'),
+('IS154', 'C105', 'The Road', CURRENT_DATE - INTERVAL '32 days',  '978-0-375-50167-0', 'E101');
+
+-- ADDING NEW COLUMN TO RETURN STATUS TABLE
+ALTER TABLE return_status
+ADD Column book_quality VARCHAR(15) DEFAULT('Good');
+
+-- UPDATING BOOK QUALITY FOR SPECIFIC RETURNED BOOKS
+UPDATE return_status
+SET book_quality = 'Damaged'
+WHERE issued_id 
+    IN ('IS112', 'IS117', 'IS118');
